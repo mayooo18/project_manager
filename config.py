3033ccf9d630d13dd13e8525d0b619195d735257
Mario Mendez <mendezmario2018@gmail.com>
2025-07-01 15:13:21 -0500

    migrate to postgres

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ load_dotenv()
 class Config:
 
     SECRET_KEY = os.getenv("SECRET_KEY", "dev")
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///data.db'
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     UPLOAD_FOLDER = 'static/uploads'
     MAX_CONTENT_LENGTH = 16 * 1024 * 1024  # 16MB
