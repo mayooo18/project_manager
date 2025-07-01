babdd9f6fc0749da228bac325394a9bb7336810a
Mario Mendez <mendezmario2018@gmail.com>
2025-07-01 15:46:21 -0500

    updated hash string

--- models.py
+++ models.py
@@ -87,7 +87,7 @@ class Income(db.Model):
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), unique=True, nullable=False)
-    password_hash = db.Column(db.String(128), nullable=False)
+    password_hash = db.Column(db.String(512), nullable=False)
 
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
